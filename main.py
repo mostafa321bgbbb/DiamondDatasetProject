2867fd7c99a76560d8cc0255db9ef6591990811e
Mostafa Moharram <mostafa.moharram@robogarden.ca>
2023-11-21 12:37:40 -0500

    removed testing code from main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 ########################To be implemented####################################
-from Analyzer import Analyzer
-
-analyzer = Analyzer()
-analyzer.read_dataset('diamonds.csv')
-#analyzer.describe()
-analyzer.drop_missing_data()
-analyzer.plot_histograms_numeric('carat')
